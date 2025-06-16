4b371d6f03504d5a31f979bf118b30cbd720451d
Shirin Zahra <shirin@bayo.com.my>
2025-06-16 18:31:42 +0800

    test add link

--- portfolio/script.js
+++ portfolio/script.js
@@ -5,3 +5,10 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navBar.classList.toggle('active');
 }
+
+document.querySelectorAll('.btn').forEach(btn => {
+    btn.addEventListener('click', function () {
+        const link = this.dataset.link;
+        window.open(link, '_blank');
+    });
+});
